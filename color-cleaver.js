e64bcaccef1e8890ae705883eb87bb0b1a35815a
kreit <reitenbaughkm@gmail.com>
2024-10-09 10:29:43 -0500

    On branch master  Initial commit  Changes to be committed: 	new file:   README.md 	new file:   color-cleaver.js 	new file:   color-combinator.js 	new file:   color-deconstructor.js 	new file:   get-input.js 	new file:   is-valid-primary.js 	new file:   is-valid-secondary.js 	new file:   main.test.js 	new file:   outputCheatSheet.md 	new file:   package.json

---
+++ color-cleaver.js
@@ -0,0 +1,9 @@
+const colorCombinator = require("./color-combinator.js");
+const colorDeconstructor = require("./color-deconstructor.js");
+const isValidPrimary = require("./is-valid-primary.js");
+const isValidSecondary = require("./is-valid-secondary.js");
+const getInput = require("./get-input.js");
+
+// Your code here!
+
+console.log("blue and yellow make green")
